c10c38d83556edab674175c25d9ff5b851c4ce21
reubsim <21019352@myrp.ude.sg>
2023-05-17 16:42:07 +0800

    First Commit

---
+++ app/src/main/java/com/example/pepperbot/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pepperbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
