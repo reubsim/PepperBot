1bd3929fac76e890935ed85c3f84e749f8bd5119
hay-kal <21040411@myrp.edu.sg>
2023-05-30 22:43:03 +0800

    Suhaify Module Completed - Haykal

--- welcome/src/main/java/com/example/welcome/feedback.java
+++ welcome/src/main/java/com/example/welcome/feedback.java
@@ -4,11 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class feedback extends AppCompatActivity {
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 
+public class feedback extends AppCompatActivity implements RobotLifecycleCallbacks {
+
+    private QiContext qiContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (qiContext != null) {
+            feedback();
+        }
+    }
+
+    private void feedback() {
+        if (qiContext != null) {
+            Say say = SayBuilder.with(qiContext)
+                    .withText("Please leave us feedback!")
+                    .build();
+
+            say.async().run();
+        }
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        this.qiContext = qiContext;
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // Release the QiContext
+        this.qiContext = null;
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // Handle focus refusal
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        QiSDK.unregister(this, this);
     }
 }
