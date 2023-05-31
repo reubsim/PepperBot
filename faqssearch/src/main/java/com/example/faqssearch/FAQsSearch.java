84f1dfc1eac2f2d01b3b418d42ec22b2cea5005e
reubsim <21019352@myrp.ude.sg>
2023-05-31 11:30:03 +0800

    Added FAQ (includes options and search), Lost & Found

---
+++ faqssearch/src/main/java/com/example/faqssearch/FAQsSearch.java
@@ -0,0 +1,14 @@
+package com.example.faqssearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FAQsSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faqs_search);
+    }
+}
---
+++ lostnfound/src/main/java/com/example/lostnfound/LostnFound.java
@@ -0,0 +1,14 @@
+package com.example.lostnfound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LostnFound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lostn_found);
+    }
+}
